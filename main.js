bf9c17d8c552a684bd2e84bf9d5928ef1d9074e1
HR <adrijroy99@gmail.coim>
2020-07-23 17:34:17 +0600

    first

---
+++ main.js
@@ -0,0 +1,49 @@
+//Created Function
+// Function for Plus Button
+function updateByPlus(plusId, inputId, priceId){
+var currentPrice = parseFloat(document.getElementById(priceId).innerText);
+    const plusBtn = document.getElementById(plusId);
+plusBtn.addEventListener('click', function(){
+    var input = parseFloat(document.getElementById(inputId).value);
+    if(input>=0){
+        input++;
+    }
+    document.getElementById(inputId).value = input;
+    document.getElementById(priceId).innerText = currentPrice * input;
+    
+    sumarry();    
+})
+}
+// Function for Minus Button
+function updateByMinus(minusId, inputId, priceId){
+    var currentPrice = parseFloat(document.getElementById(priceId).innerText);
+    const minusBtn = document.getElementById(minusId);
+    minusBtn.addEventListener('click', function(){
+    var input = parseFloat(document.getElementById(inputId).value);
+    if(input>0){
+    input--;
+    }
+    document.getElementById(inputId).value = input;
+    document.getElementById(priceId).innerText = currentPrice * input;
+    
+    sumarry();
+    })
+    }
+//Function for Fotter
+function sumarry(){
+    const phnT = parseFloat(document.getElementById('phn-price').innerText);
+    const caseT = parseFloat(document.getElementById('case-price').innerText); 
+    
+    const tax = parseFloat(document.getElementById('tax').innerText);
+    document.getElementById('sub-total').innerText = phnT + caseT;
+    document.getElementById('total').innerText = phnT + caseT + tax;
+}
+
+//Created Function
+
+updateByPlus("phn-plus", "phn-input", "phn-price");
+updateByMinus("phn-minus", "phn-input", "phn-price");
+updateByPlus("case-plus", "case-input", "case-price");
+updateByMinus("case-minus", "case-input", "case-price");
+
+
